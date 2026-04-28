33857c1f43c4a9e2a7e59ad3fb0037020850a457
Thierry <thierrypitela@hotmail.com>
2026-04-27 22:09:46 -0300

    refactor: Melhorando a função findPostBySlugCached para reuso

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,4 +1,5 @@
 import { jsonPostRepository } from '@/repositories/post';
+import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
 // Cache the query to avoid re-fetching the data on every render
@@ -10,6 +11,10 @@ export const findPostBySlugCached = cache(
   async (slug: string) => await jsonPostRepository.findBySlug(slug),
 );
 
-export const findPostByIdCached = cache(
-  async (id: string) => await jsonPostRepository.findById(id),
-);
+export const findPostByIdCached = cache(async (slug: string) => {
+  const post = await jsonPostRepository.findBySlug(slug).catch(() => undefined);
+
+  if (!post) notFound();
+
+  return post;
+});
