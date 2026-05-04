b9f0357af9b18688e5ef3e609a3a4378d15c7738
Thierry <thierrypitela@hotmail.com>
2026-05-04 09:47:58 -0300

    feat: Usando Suspense para garantir o carregando rápido da página mesmo sem conteúdo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,11 @@ import { Suspense } from "react";
 import SpinLoader from "@components/SpinLoader";
 import PostsList from "@components/PostsList";
 import { PostFeatured } from "@/components/PostFeatured";
-
 export default function Home() {
   return (
     <>
-      <Suspense fallback={<SpinLoader />}>
+      <Suspense fallback={<SpinLoader containerClassName='min-h-20 mb-16' />}>
         <PostFeatured />
-      </Suspense>
-
-      <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
     </>
