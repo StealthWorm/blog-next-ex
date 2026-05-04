3b130b825105feaab0f9be9b2fc86fff2884c7e9
Thierry <thierrypitela@hotmail.com>
2026-05-04 10:40:29 -0300

    refactor: Análise de cuidados  para não mudar um Server Component em Client Component sem querer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import { Suspense } from "react";
 import SpinLoader from "@components/SpinLoader";
 import PostsList from "@components/PostsList";
 import { PostFeatured } from "@/components/PostFeatured";
+import { ServerComponent } from "@/components/ServerComponent";
+import { ClientComponent } from "@/components/ClientComponent";
+
 export default function Home() {
   return (
     <>
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+
       <Suspense fallback={<SpinLoader containerClassName='min-h-20 mb-16' />}>
         <PostFeatured />
         <PostsList />
